f34f6b415114202d5c061e98bbb4c93108b5cad8
grzesiekzemlik-web <grzesiek.zemlik@icloud.com>
2026-07-22 14:39:15 +0200

    Update charts.js

--- charts.js
+++ charts.js
@@ -1,1 +1,99 @@
+// Kapitał AI - wykresy
 
+
+let chart;
+
+
+
+function updateChart(){
+
+
+let canvas =
+document.getElementById("expenseChart");
+
+
+
+if(!canvas){
+
+return;
+
+}
+
+
+
+let categories = {};
+
+
+
+expenses.forEach(e=>{
+
+
+if(!categories[e.category]){
+
+categories[e.category]=0;
+
+}
+
+
+categories[e.category]+=e.amount;
+
+
+});
+
+
+
+let labels =
+Object.keys(categories);
+
+
+
+let values =
+Object.values(categories);
+
+
+
+if(chart){
+
+chart.destroy();
+
+}
+
+
+
+chart = new Chart(canvas, {
+
+
+type:"doughnut",
+
+
+data:{
+
+
+labels:labels,
+
+
+datasets:[{
+
+data:values
+
+}]
+
+
+},
+
+
+options:{
+
+
+responsive:true
+
+
+}
+
+
+
+});
+
+
+
+}
