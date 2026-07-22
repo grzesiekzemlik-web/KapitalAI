afa571db875ff5ff1889f62499e2d8a897c86228
grzesiekzemlik-web <grzesiek.zemlik@icloud.com>
2026-07-22 14:22:11 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,65 @@
+// Kapitał AI - pamięć aplikacji
 
+
+function loadMoney(){
+
+return Number(localStorage.getItem("money")) || 0;
+
+}
+
+
+
+function saveMoney(value){
+
+localStorage.setItem(
+"money",
+value
+);
+
+}
+
+
+
+
+
+function loadExpenses(){
+
+return JSON.parse(
+localStorage.getItem("expenses")
+) || [];
+
+}
+
+
+
+function saveExpenses(data){
+
+localStorage.setItem(
+"expenses",
+JSON.stringify(data)
+);
+
+}
+
+
+
+
+
+function loadGoal(){
+
+return JSON.parse(
+localStorage.getItem("goal")
+) || null;
+
+}
+
+
+
+function saveGoalData(goal){
+
+localStorage.setItem(
+"goal",
+JSON.stringify(goal)
+);
+
+}
